034726d75bb11b802fb6bb6b8ff0397e61d062f4
fulya_ture <fulyature@gmail.com>
2023-07-04 17:48:27 +0300

    props

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import Card from "./components/Card";
-import Header from "./components/Header";
+// import Header from "./components/Header";
 import { contacts } from "./data";
+import "bootstrap/dist/css/bootstrap.css";
 
 function App() {
   return (
-    <div className="App">
-      <Card contacts={contacts} />
-      <Header />
+    <div className="container row">
+      <h1 className="text-danger text-center m-4 border-bottom display-1">
+        FullVitamin Team
+      </h1>
+      {contacts.map((contact) => {
+        const { id, name, imgURL, email, phone } = contact;
+        return (
+          <div key={id}>
+            <Card
+              id={id}
+              name={name}
+              imgUrl={imgURL}
+              email={email}
+              phone={phone}
+            />
+          </div>
+        );
+      })}
     </div>
   );
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,5 +1,17 @@
-const Card = (props) => {
-  return <div>Card</div>;
+const Card = ({ id, name, imgURL, phone, email }) => {
+  return (
+    <div className="card col col-sm-6 col-md-4 col-lg-3 text-center p-3 ">
+      <h5 className="card-title text-danger">{name}</h5>
+      <img
+        src={imgURL}
+        className="card-img-top rounded-circle bg-danger w-75 mx-auto"
+        alt="avatar"
+      />
+      <div className="card-body">
+        <p className="card-text text-success">{phone}</p>
+        <p className="card-text text-success">{email}</p>
+      </div>
+    </div>
+  );
 };
-
 export default Card;
